4af88170a126987398e676ce6429a183c1e71710
mlangeveld15 <36929407+mlangeveld15@users.noreply.github.com>
2018-09-26 22:03:00 +0200

    push

---
+++ api/BLL/voidUser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nsp
+ * Date: 2018/06/21
+ * Time: 18:00
+ */
+require_once '../BLL/CORS_Headers.php';
+require_once '../DAL/DBHandler.php';
+
+use DAL\DBHandler;
+
+$action = $_GET['action'];
+
+if ($action == 'update') {
+    $json = json_decode(file_get_contents('php://input'));
+    echo json_encode(DBHandler::VoidUser_Status($json->UserID, $json->Status));
+} else if ($action == 'type') {
+    $json = json_decode(file_get_contents('php://input'));
+    echo json_encode(DBHandler::VoidUser_Type($json->UserID, $json->Status));
+} else if ($action == 'users') {
+    $userID = $_GET['userID'];
+    echo json_encode(DBHandler::VoidUser_Users($userID));
+}
